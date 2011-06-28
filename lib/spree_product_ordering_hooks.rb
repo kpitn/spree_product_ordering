0518e2459c53a1f34da472dd264e38000e3bf919
Pierre <pierre.basile@gmail.com>
2011-06-28 22:56:17 +0200

    add french locale

--- lib/spree_product_ordering_hooks.rb
+++ lib/spree_product_ordering_hooks.rb
@@ -1,3 +1,8 @@
 class SpreeProductOrderingHooks < Spree::ThemeSupport::HookListener
-  insert_after :admin_product_sub_tabs,   'admin/hooks/sub_menu_tab'
+  Deface::Override.new(:virtual_path => "admin/shared/_product_sub_menu",
+                       :name => "converted_admin_product_sub_tabs_239949077",
+                       :insert_bottom => "[data-hook='admin_product_sub_tabs'], #admin_product_sub_tabs[data-hook]",
+                       :partial => "admin/hooks/sub_menu_tab",
+                       :disabled => false)
+
 end
